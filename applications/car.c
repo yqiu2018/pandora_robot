e2603bdc1a0a21211557a7edcc738408431d7e5c
sogwms <sogwyms@gamil.com>
2019-10-09 20:04:58 +0800

    update app  based on the latest package of rt-robot

--- applications/car.c
+++ applications/car.c
@@ -2,7 +2,6 @@
 #include <chassis.h>
 #include <command.h>
 #include <ps2.h>
-#include <ano.h>
 #include <dual_pwm_motor.h>
 #include <ab_phase_encoder.h>
 #include <inc_pid_controller.h>
@@ -28,9 +27,9 @@
 #define RIGHT_ENCODER_A_PHASE_PIN   38      // GET_PIN(C, 6)
 #define RIGHT_ENCODER_B_PHASE_PIN   39      // GET_PIN(C, 7)
 #define PULSE_PER_REVOL           2000      // Real value 2000
-#define SAMPLE_TIME                 50
+#define ENCODER_SAMPLE_TIME         50
 
-// CONTROLLER PID
+// PID CONTROLLER
 #define PID_SAMPLE_TIME             50
 #define PID_PARAM_KP                6.6
 #define PID_PARAM_KI                6.5
@@ -46,16 +45,16 @@ chassis_t chas;
 #define WHEEL_DIST_X                 0
 #define WHEEL_DIST_Y              0.13
 
+// PS2
+#define PS2_CS_PIN                  28
+#define PS2_CLK_PIN                 29
+#define PS2_DO_PIN                   4
+#define PS2_DI_PIN                  36
+
 // Car Thread
 #define THREAD_PRIORITY             10
 #define THREAD_STACK_SIZE          512
 #define THREAD_TIMESLICE             5
-
-static rt_err_t car_forward(rt_int8_t cmd, void *param);
-static rt_err_t car_backward(rt_int8_t cmd, void *param);
-static rt_err_t car_turnleft(rt_int8_t cmd, void *param);
-static rt_err_t car_turnright(rt_int8_t cmd, void *param);
-static rt_err_t car_stop(rt_int8_t cmd, void *param);
 
 static rt_thread_t tid_car = RT_NULL;
 
@@ -70,15 +69,14 @@ void car_thread(void *param)
     target_velocity.angular_z = 0;
     chassis_set_velocity(chas, target_velocity);
 
-    // Open control
-    // auto_control_disable(chas->c_wheels[0]->w_control);
-    // auto_control_disable(chas->c_wheels[1]->w_control);
+    // Open-loop control
+    // controller_disable(chas->c_wheels[0]->w_controller);
+    // controller_disable(chas->c_wheels[1]->w_controller);
 
     while (1)
     {
-        rt_thread_mdelay(50);
+        rt_thread_mdelay(ENCODER_SAMPLE_TIME);
         chassis_update(chas);
-        // ano_send_senser(chas->c_wheels[0]->rpm, chas->c_wheels[0]->w_controller->target, chas->c_wheels[1]->rpm, chas->c_wheels[1]->w_controller->target,0,0,0,0,0,0);
     }
 
 //    chassis_destroy(chas);
@@ -98,12 +96,12 @@ void car_init(void *parameter)
     dual_pwm_motor_t right_motor  = dual_pwm_motor_create(RIGHT_FORWARD_PWM, RIGHT_FORWARD_PWM_CHANNEL, RIGHT_BACKWARD_PWM, RIGHT_BACKWARD_PWM_CHANNEL);
 
     // 1.2 Create two encoders
-    ab_phase_encoder_t left_encoder  = ab_phase_encoder_create(LEFT_ENCODER_A_PHASE_PIN, LEFT_ENCODER_B_PHASE_PIN, PULSE_PER_REVOL);
-    ab_phase_encoder_t right_encoder = ab_phase_encoder_create(RIGHT_ENCODER_A_PHASE_PIN, RIGHT_ENCODER_B_PHASE_PIN, PULSE_PER_REVOL);
+    ab_phase_encoder_t left_encoder  = ab_phase_encoder_create(LEFT_ENCODER_A_PHASE_PIN, LEFT_ENCODER_B_PHASE_PIN, PULSE_PER_REVOL, ENCODER_SAMPLE_TIME);
+    ab_phase_encoder_t right_encoder = ab_phase_encoder_create(RIGHT_ENCODER_A_PHASE_PIN, RIGHT_ENCODER_B_PHASE_PIN, PULSE_PER_REVOL, ENCODER_SAMPLE_TIME);
 
     // 1.3 Create two pid contollers
-    inc_pid_controller_t left_pid  = inc_pid_controller_create(PID_PARAM_KP, PID_PARAM_KI, PID_PARAM_KD);
-    inc_pid_controller_t right_pid = inc_pid_controller_create(PID_PARAM_KP, PID_PARAM_KI, PID_PARAM_KD);
+    inc_pid_controller_t left_pid  = inc_pid_controller_create(PID_PARAM_KP, PID_PARAM_KI, PID_PARAM_KD, PID_SAMPLE_TIME);
+    inc_pid_controller_t right_pid = inc_pid_controller_create(PID_PARAM_KP, PID_PARAM_KI, PID_PARAM_KD, PID_SAMPLE_TIME);
 
     // 1.4 Add two wheels
     c_wheels[0] = wheel_create((motor_t)left_motor,  (encoder_t)left_encoder,  (controller_t)left_pid,  WHEEL_RADIUS, GEAR_RATIO);
@@ -115,26 +113,13 @@ void car_init(void *parameter)
     // 3. Initialize Chassis
     chas = chassis_create(c_wheels, c_kinematics);
 
-    // Set Sample time
-    encoder_set_sample_time(chas->c_wheels[0]->w_encoder, SAMPLE_TIME);
-    encoder_set_sample_time(chas->c_wheels[1]->w_encoder, SAMPLE_TIME);
-    controller_set_sample_time(chas->c_wheels[0]->w_controller, PID_SAMPLE_TIME);
-    controller_set_sample_time(chas->c_wheels[1]->w_controller, PID_SAMPLE_TIME);
-
     // 4. Enable Chassis
     chassis_enable(chas);
 
-    // Register command
-    command_register(COMMAND_CAR_STOP     , car_stop);
-    command_register(COMMAND_CAR_FORWARD  , car_forward);
-    command_register(COMMAND_CAR_BACKWARD , car_backward);
-    command_register(COMMAND_CAR_TURNLEFT , car_turnleft);
-    command_register(COMMAND_CAR_TURNRIGHT, car_turnright);
+    // Remote-control
+    command_init(chas);
 
-    rt_kprintf("car command register complete\n");
-
-    // Controller
-    ps2_init(28, 29, 4, 36);
+    ps2_init(PS2_CS_PIN, PS2_CLK_PIN, PS2_DO_PIN, PS2_DI_PIN);
 
     // thread
     tid_car = rt_thread_create("tcar",
@@ -147,77 +132,3 @@ void car_init(void *parameter)
         rt_thread_startup(tid_car);
     }
 }
-
-static rt_err_t car_forward(rt_int8_t cmd, void *param)
-{
-    struct velocity target_velocity;
-
-    target_velocity.linear_x = 0.05f;
-    target_velocity.linear_y = 0;
-    target_velocity.angular_z = 0;
-    chassis_set_velocity(chas, target_velocity);
-
-    LOG_D("forward cmd");
-
-    return RT_EOK;
-}
-
-static rt_err_t car_backward(rt_int8_t cmd, void *param)
-{
-    struct velocity target_velocity;
-
-    target_velocity.linear_x = -0.05f;
-    target_velocity.linear_y = 0;
-    target_velocity.angular_z = 0;
-    chassis_set_velocity(chas, target_velocity);
-
-    LOG_D("backward cmd");
-
-    return RT_EOK;
-}
-
-static rt_err_t car_turnleft(rt_int8_t cmd, void *param)
-{
-    struct velocity target_velocity;
-
-    target_velocity.linear_x = 0.00f;
-    target_velocity.linear_y = 0;
-    target_velocity.angular_z = 0.5;
-    chassis_set_velocity(chas, target_velocity);
-
-    LOG_D("turnleft cmd");
-
-    return RT_EOK;
-}
-
-static rt_err_t car_turnright(rt_int8_t cmd, void *param)
-{
-    struct velocity target_velocity;
-
-    target_velocity.linear_x = 0.00f;
-    target_velocity.linear_y = 0;
-    target_velocity.angular_z = -0.5;
-    chassis_set_velocity(chas, target_velocity);
-
-    LOG_D("turnright cmd");
-
-    return RT_EOK;
-}
-
-static rt_err_t car_stop(rt_int8_t cmd, void *param)
-{
-    struct velocity target_velocity;
-
-    target_velocity.linear_x = 0.00f;
-    target_velocity.linear_y = 0;
-    target_velocity.angular_z = 0;
-    chassis_set_velocity(chas, target_velocity);
-
-    LOG_D("stop cmd");
-
-    return RT_EOK;
-}
-
-
-
-
